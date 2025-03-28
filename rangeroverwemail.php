0cbc5a5c7ff87ee0cafac18e8ce36fab5115489a
gamestorelife <freerapper666@gmail.com>
2025-03-28 19:51:39 +0200

    update the Link Image
    update the Link Image inside the Welcome Email Links HTML

--- rangeroverwemail.php
+++ rangeroverwemail.php
@@ -2,53 +2,52 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\PHPMailerAutoload;
-
-//require 'vendor/autoload.php';
 
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
-require 'PHPMailer-master/PHPMailerAutoload.php';
 
 // Retrieve form data
-
 $email = $_POST['email'];
-//$sender_email = $_POST['sender_email'];
-//$subject = $_POST['subject'];
-//$message = $_POST['message'];
 
-// Create a new PHPMailer instance
-$mail = new PHPMailer(true);
-
-
+try {
+    // Create a single PHPMailer instance
+    $mail = new PHPMailer(true);
+    
     // Server settings
-    $mail = new PHPMailer();
-   $mail->isSMTP();
-   $mail->Host = 'smtp.hostinger.com';
-   $mail->Port = 465;
-   $mail->SMTPAuth = true;
-   $mail->Username = 'no-reply@eliteclasstransfers.com';
-   $mail->Password = 'Noeliteclasstransfers23**';
-   $mail->SMTPSecure = 'ssl';                                 // TCP port to connect to
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 465;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'no-reply@eliteclasstransfers.com';
+    $mail->Password = 'Noeliteclasstransfers23**';
+    $mail->SMTPSecure = 'ssl';
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
 
     // Recipients
     $mail->setFrom('no-reply@eliteclasstransfers.com', 'Elite Class Transfers');
-    $mail->addAddress($email);     // Add a recipient
-
+    $mail->addAddress($email);
+    
+    // Content
+    $mail->isHTML(true);
     $mail->Subject = 'Welcome to Elite Class Transfers';
-    $mail->Body = 'Thank you For Elite Class Transfers';
-     $mail->msgHTML(file_get_contents('rangerovermessag.html'), __DIR__);
+    
+    // Load HTML content from file
+    $htmlFile = 'rangerovermessage.html';
+    if (file_exists($htmlFile)) {
+        $mail->msgHTML(file_get_contents($htmlFile), __DIR__);
+    } else {
+        $mail->Body = 'Thank you For Elite Class Transfers';
+        error_log("HTML file not found: " . $htmlFile);
+    }
 
-
-
-   if(!$mail->send()) {
-    echo 'Email could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo '<center><h1>Email has been sent.</h1></center>';
+    if(!$mail->send()) {
+        echo 'Email could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo '<center><h1>Email has been sent.</h1></center>';
+    }
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-
-
 ?>
